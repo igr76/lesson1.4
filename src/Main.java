08efe01adffbd524f5b7eb02bef95f73194f3522
igr1 <dfrgth>
2022-08-26 08:33:45 +1000

    урок 1.4

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        for (int i=1; i < 11; i++) {
-            System.out.print(i + " ");
+        int x=1;
+
+        while ( x < 11) {
+            System.out.print(x + " ");
+            x++;
         }
        System.out.println("");
 
